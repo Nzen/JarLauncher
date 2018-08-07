ab1ea545a290f555cadd145c1b37e6cd9da080c9
Nicholas Prado <nmprado@nzen.ws>
2018-08-06 23:50:16 -0500

    Java9 module descriptor, hardcoded compile script
    Until I ensure this can use maven or gradle to make modular jars,
    a script with a hardcoded javac path will have to do.

---
+++ notgradle.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+/usr/lib/jvm/java-10-openjdk-amd64/bin/javac \
+	-d mods/ws.nzen.jarl \
+	src/main/java/ws.nzen.jarl/module-info.java \
+	src/main/java/ws/nzen/jarl/JarLauncher.java src/main/java/ws/nzen/jarl/Launcher.java \
+	src/main/java/ws/nzen/jarl/model/ArgBundle.java src/main/java/ws/nzen/jarl/model/JarLocation.java src/main/java/ws/nzen/jarl/model/JarModel.java src/main/java/ws/nzen/jarl/model/SelfDescribes.java \
+	src/main/java/ws/nzen/jarl/parser/ConfigParser.java src/main/java/ws/nzen/jarl/parser/FastArgParser.java src/main/java/ws/nzen/jarl/parser/ParserFactory.java src/main/java/ws/nzen/jarl/parser/XmlBasedParser.java \
+	src/main/java/ws/nzen/jarl/ui/CliSelection.java src/main/java/ws/nzen/jarl/ui/SelectionUi.java
+
+/usr/lib/jvm/java-10-openjdk-amd64/bin/jar --create --file=mlib/ws.nzen.jarl@0.0.jar --module-version=1.0 -C mods/ws.nzen.jarl .
---
+++ src/main/java/ws.nzen.jarl/module-info.java
@@ -0,0 +1,7 @@
+
+module ws.nzen.jarl
+{
+	requires java.desktop;
+	requires java.xml;
+	exports ws.nzen.jarl;
+}
